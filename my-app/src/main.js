3720f795270c47a11dd72ca26d9c74e5bace3c4e
Aaron Ocampo <aocampo@externo.infonavit.org.mx>
2021-03-10 21:51:37 -0600

    login y routing

--- my-app/src/main.js
+++ my-app/src/main.js
@@ -7,16 +7,20 @@ import 'vuetify/dist/vuetify.min.css'
 import VueQRCodeComponent from 'vue-qrcode-component'
 import DatetimePicker from 'vuetify-datetime-picker'
 import '@mdi/font/css/materialdesignicons.css' 
+import VueRouter from 'vue-router'
+import router from './router'
 Vue.component('qr-code', VueQRCodeComponent)
-Vue.use(Vuetify);
-Vue.use(VuetifyIco);
+Vue.use(Vuetify)
+Vue.use(VuetifyIco)
 Vue.use(DatetimePicker)
+Vue.use(VueRouter)
 
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router,
   vuetify : new Vuetify(),
 }).$mount('#app')
 
